fd6f39bebb8a837fe1ffa225b538b22b466f8ae6
Firepup Sixfifty <firepyp650@gmail.com>
2024-02-10 12:40:52 -0600

    Attempt to modernize seting up components

---
+++ javascripts/discourse/connectors/user-card-metadata/custom-profile-link.js
@@ -0,0 +1,26 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+
+export default class CustomProfileLink extends Component {
+    get links() {
+        if (settings.custom_profile_link_debug_mode) console.debug("[Custom Profile Link] Settings dump follows", settings);
+        const ids = settings.custom_profile_link_user_field_ids.replace(/_/g, "").split(/\|/).map(Number);
+        const labels = settings.custom_profile_link_labels.split(/\|/);
+        const prefixes = settings.custom_profile_link_prefixes.split(/\|/);
+        const orgs = settings.custom_profile_link_orgs.split(/\|/);
+        const parsedSettings = { "ids": ids, "labels": labels, "prefixes": prefixes /*, "orgs": {"labels": orgLabels, "name-ids": orgNames, "url-ids": orgUrls} */ }
+        if (settings.custom_profile_link_debug_mode) console.debug("[Custom Profile Link] Parsed settings dump follows", parsedSettings);
+        let links = [];
+        if (!args.model.get('user_fields')) {
+            console.warn(`[Custom Profile Link] User Card () missing "user_fields"! Raw user dump follows.`, args.model);
+            return undefined;
+        } else {
+            for (let i = 0; i < ids.length; i++) {
+                if (!args.model.get('user_fields')[ids[i]] && settings.custom_profile_link_debug_mode) console.debug(`[Custom Profile Link] User field ${ids[i]} missing. user_fields dump follows.`, args.model.get("user_fields"));
+                if (!!args.model.get('user_fields')[ids[i]]) links.push([args.model.get('user_fields')[ids[i]], labels[i], prefixes[i]]);
+            }
+            if (settings.custom_profile_link_debug_mode) console.debug("[Custom Profile Link] links built, dump:", links)
+            return links;
+        }
+    }
+}
