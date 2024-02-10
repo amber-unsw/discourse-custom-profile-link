6b81fced335bdc5e95e3fbcc6e87ac5194a80452
Firepup Sixfifty <firepyp650@gmail.com>
2024-02-10 13:05:03 -0600

    invert these I guess

--- javascripts/discourse/connectors/user-card-metadata/custom-profile-link.js
+++ javascripts/discourse/connectors/user-card-metadata/custom-profile-link.js
@@ -12,13 +12,13 @@ export default class CustomProfileLink extends Component {
         if (settings.custom_profile_link_debug_mode) console.debug("[Custom Profile Link] Parsed settings dump follows", parsedSettings);
         let links = [];
         if (settings.custom_profile_link_debug_mode) console.debug("[Custom Profile Link] args dump:", this.args.outletArgs);
-        if (!this.args.outletArgs.model.get('user_fields')) {
-            console.warn(`[Custom Profile Link] User Card () missing "user_fields"! Raw user dump follows.`, this.args.outletArgs.model);
+        if (!this.args.outletArgs.user.get('user_fields')) {
+            console.warn(`[Custom Profile Link] User Card () missing "user_fields"! Raw user dump follows.`, this.args.outletArgs.user);
             return undefined;
         } else {
             for (let i = 0; i < ids.length; i++) {
-                if (!this.args.outletArgs.model.get('user_fields')[ids[i]] && settings.custom_profile_link_debug_mode) console.debug(`[Custom Profile Link] User field ${ids[i]} missing. user_fields dump follows.`, this.args.outletArgs.model.get("user_fields"));
-                if (!!this.args.outletArgs.model.get('user_fields')[ids[i]]) links.push([this.args.outletArgs.model.get('user_fields')[ids[i]], labels[i], prefixes[i]]);
+                if (!this.args.outletArgs.user.get('user_fields')[ids[i]] && settings.custom_profile_link_debug_mode) console.debug(`[Custom Profile Link] User field ${ids[i]} missing. user_fields dump follows.`, this.args.outletArgs.user.get("user_fields"));
+                if (!!this.args.outletArgs.user.get('user_fields')[ids[i]]) links.push([this.args.outletArgs.user.get('user_fields')[ids[i]], labels[i], prefixes[i]]);
             }
             if (settings.custom_profile_link_debug_mode) console.debug("[Custom Profile Link] links built, dump:", links);
             return links;
